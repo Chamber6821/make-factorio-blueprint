b862ee3005e05dd4ecc3d301deee476732030033
Chamber6821 <vania.osinin@yandex.ru>
2022-03-18 18:22:54 +0300

    feat: add all vanilla virtual signals

--- src/Signal.ts
+++ src/Signal.ts
@@ -1,4 +1,18 @@
+type Letter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K"
+    | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X"
+    | "Y" | "Z"
+
+type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
+type Color = "red" | "green" | "blue" | "yellow" | "pink" | "cyan" | "white" | "grey" | "black"
+
+type Special = "check" | "info" | "dot"
+
 export default class Signal {
+    static virtual(postName: Letter | Digit | Color | Special) {
+        return new Signal("virtual", `signal-${postName}`)
+    }
+
     constructor(
         readonly type: "item" | "fluid" | "virtual",
         readonly name: string
--- src/utils/createLetterSignal.ts
+++
@@ -1,6 +0,0 @@
-import Signal from "../Signal"
-
-
-export default function createLetterSignal(char: string): Signal {
-    return new Signal("virtual", `signal-${char}`)
-}
